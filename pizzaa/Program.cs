ef205ddef7dc6641fb41da8f752290996186da33
vika <malyovanavika@gmail.com>
2025-02-25 12:33:41 +0200

    add menu vika

--- pizzaa/Program.cs
+++ pizzaa/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("дарова");
+Console.WriteLine("------------ Menu -----------");
+Console.WriteLine("1. Add new pizza");
+Console.WriteLine("2. Add new product");
+Console.WriteLine("3. Save pizzas data in a file");
+Console.WriteLine("4. Download pizzas data from a file ");
+Console.WriteLine("5. Observe finished pizzas");
+Console.WriteLine("6. Find by ingredientі");
+Console.WriteLine("7. Pizza order from client, including possible creating of custom pizza");
+Console.WriteLine("8. Calculate a client (bill)");
